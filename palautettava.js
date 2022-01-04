e56ffbd082f5adb81504698e4737138125c76156
CaritaNi <93704532+CaritaNi@users.noreply.github.com>
2022-01-05 00:20:09 +0200

    Interaktio toimii

--- palautettava.js
+++ palautettava.js
@@ -1,5 +1,23 @@
-let xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET","https://andruxnet-random-famous-quotes.p.rapidapi.com/",true);
-xmlhttp.send();
+function quote(){
 
+    const data = null;
+
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+            
+            const myJson = JSON.parse(this.responseText);
+            document.getElementById("printHere").innerHTML ="''" + myJson[1].text + "''" + " - " + myJson[1].author;
+        }
+    });
+
+    xhr.open("GET", "https://famous-quotes4.p.rapidapi.com/random?category=all&count=2");
+    xhr.setRequestHeader("x-rapidapi-host", "famous-quotes4.p.rapidapi.com");
+    xhr.setRequestHeader("x-rapidapi-key", "40c48fa6damsha0a8ae0f2ddbcefp1bbd92jsn1e37c3f2b1ed");
+
+    xhr.send(data);
+}
 
